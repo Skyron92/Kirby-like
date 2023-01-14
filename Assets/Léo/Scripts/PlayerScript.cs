9f5f471d698a4636540fd2788aa15974a038f03d
Skyron92 <courtier.leo@gmail.com>
2023-01-14 18:37:14 +0100

    Tornade de merde

--- Assets/Léo/Scripts/PlayerScript.cs
+++ Assets/Léo/Scripts/PlayerScript.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class PlayerScript : MonoBehaviour
 {
-    // Start is called before the first frame update
+    private CharacterController _controller;
     void Start()
     {
         
